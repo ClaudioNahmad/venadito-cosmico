d240754732e15ceae44fb442c9506f965557b69e
ClaudioNahmad <claudio.nahmad@ciencias.unam.mx>
2018-05-11 14:50:45 -0500

    modifying w integration

--- 00_hikuri_executables/integration_w.c
+++ 00_hikuri_executables/integration_w.c
@@ -16,22 +16,27 @@
 
 /* w(a) */
 int a_function(
-           struct background *pba,
            double a,
            double * a_trans,
            double * a_func) {
 
  *a_func = (1.- a / a)*(pba->a_trans / (1. - pba->a_trans));
 
-  return _SUCCESS_DEFINING_w(a)_HIKURI_;
+  return _SUCCESS_DEFINING_wa_;
 }
 
 
-/* sin*/
-double func(const double x, void *param)
-{
-    return sin(x);
-}
+int background_w_fld(
+                     double a,
+                     double q = q_factor,			
+                     double * w_fld,
+               
+                    ) {
+  /** local variables */
+  double * a_func;
+ 
+  *w_fld = w0_fld + wa_fld * ((pow(a_func, q_factor)) / (1. + pow(a_func, q_factor)));
+/**************************************************************************************************************************/
  
 int main(void)
 {
@@ -43,7 +48,7 @@ int main(void)
     /* set integration interval and integrant */
     a = 0.0;
     b = M_PI;
-    f.function = &func;
+    f.function = &background_w_fld;
     f.params = 0;
  
     /* Define type of Interpolation */
